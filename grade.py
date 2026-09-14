2cb43534b001080b4cf39dcec9635ff6b2e2c655
SeleneMoonvail <selenemoonvail.gmail.com>
2026-09-14 13:54:47 +0630

    total marks added

---
+++ grade.py
@@ -0,0 +1,15 @@
+name = input("Enter student name: ")
+
+mark1 = float(input("Enter first subject mark: "))
+mark2 = float(input("Enter second subject mark: "))
+
+total_marks = mark1 + mark2
+average = total_marks / 2
+
+print("Student Name: ", name)
+print("Total Mark: ", total_marks)
+print("Average mark: ", average)
+
+
+
+
